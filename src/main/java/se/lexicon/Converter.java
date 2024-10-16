ffba1c1e29020d1f8d71caadc4ec50964e4eeca8
Fatemeh <f.khalleghi.777@gmail.com>
2024-10-16 09:52:08 +0200

    converter options

---
+++ src/main/java/se/lexicon/Converter.java
@@ -0,0 +1,14 @@
+package se.lexicon;
+
+public class Converter {
+
+    public static void showMenu() {
+        System.out.println("Currency Converter:");
+        System.out.println("1. Convert SEK to USD");
+        System.out.println("2. Convert USD to SEK");
+        System.out.println("3. Convert SEK to Euro");
+        System.out.println("4. Convert Euro to SEK");
+        System.out.println("5. Exit");
+        System.out.println("Choose one of the options:");
+    }
+}
