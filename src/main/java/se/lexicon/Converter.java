6b1c993bb9d4be90d40b887eb0bbc0a4dc470af4
Fatemeh <f.khalleghi.777@gmail.com>
2024-10-16 23:12:10 +0200

    get amount

--- src/main/java/se/lexicon/Converter.java
+++ src/main/java/se/lexicon/Converter.java
@@ -11,32 +11,24 @@ public class Converter {
         System.out.println("3. Convert SEK to Euro");
         System.out.println("4. Convert Euro to SEK");
         System.out.println("5. Exit");
-        System.out.println("Choose one of the options:");
-
-        Scanner sc = new Scanner(System.in);
-        int choice = sc.nextInt();
-        switch(choice) {
-            case 1:
-                System.out.println("Enter currency symbol:");
-                String currency = sc.next();
-                break;
-            case 2:
-                System.out.println("Enter currency symbol:");
-                String currency2 = sc.next();
-                break;
-            case 3:
-                System.out.println("Enter currency symbol:");
-                String currency3 = sc.next();
-                break;
-            case 4:
-                System.out.println("Enter currency symbol:");
-                String currency4 = sc.next();
-                break;
-            case 5:
-                System.exit(0);
-            default:
-
-        }
     }
 
+    public static double getAmount(String message) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(message);
+        double amount = scanner.nextDouble();
+        return amount;
+    }
+
+
+
+
+
 }
+
+
+
+
+
+
+
